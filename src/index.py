51b58b164f76bfb77e43944eabd84feb20743d98
Tamás Tóth <tomjtoth@gmail.com>
2023-11-05 21:01:21 +0200

    pylint score 10/10

--- src/index.py
+++ src/index.py
@@ -1,3 +1,4 @@
+from textwrap import dedent
 from varasto import Varasto
 
 
@@ -5,53 +6,61 @@ def main():
     mehua = Varasto(100.0)
     olutta = Varasto(100.0, 20.2)
 
-    print("Luonnin jälkeen:")
-    print(f"Mehuvarasto: {mehua}")
-    print(f"Olutvarasto: {olutta}")
+    print(dedent(
+        f"""\
+        Luonnin jälkeen:
+        Mehuvarasto: {mehua}
+        Olutvarasto: {olutta}
+        Olut getterit:
+        saldo = {olutta.saldo}
+        tilavuus = {olutta.tilavuus}
+        paljonko_mahtuu = {olutta.paljonko_mahtuu()}
+        Mehu setterit:
+        Lisätään 50.7"""
+    ))
 
-    print("Olut getterit:")
-    print(f"saldo = {olutta.saldo}")
-    print(f"tilavuus = {olutta.tilavuus}")
-    print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
-
-    print("Mehu setterit:")
-    print("Lisätään 50.7")
     mehua.lisaa_varastoon(50.7)
-    print(f"Mehuvarasto: {mehua}")
-    print("Otetaan 3.14")
+    print(dedent(
+        f"""\
+        Mehuvarasto: {mehua}
+        Otetaan 3.14"""
+    ))
     mehua.ota_varastosta(3.14)
-    print(f"Mehuvarasto: {mehua}")
+    print(dedent(
+        f"""\
+        Mehuvarasto: {mehua}
+        Virhetilanteita:
+        Varasto(-100.0);
+        {Varasto(-100.0)}
+        Varasto(100.0, -50.7)
+        {Varasto(100.0, -50.7)}
+        Olutvarasto: {olutta}
+        olutta.lisaa_varastoon(1000.0)"""
+    ))
 
-    print("Virhetilanteita:")
-    print("Varasto(-100.0);")
-    huono = Varasto(-100.0)
-    print(huono)
-
-    print("Varasto(100.0, -50.7)")
-    huono = Varasto(100.0, -50.7)
-    print(huono)
-
-    print(f"Olutvarasto: {olutta}")
-    print("olutta.lisaa_varastoon(1000.0)")
     olutta.lisaa_varastoon(1000.0)
-    print(f"Olutvarasto: {olutta}")
 
-    print(f"Mehuvarasto: {mehua}")
-    print("mehua.lisaa_varastoon(-666.0)")
+    print(dedent(
+        f"""\
+        Olutvarasto: {olutta}
+        Mehuvarasto: {mehua}
+        mehua.lisaa_varastoon(-666.0)"""
+    ))
+
     mehua.lisaa_varastoon(-666.0)
-    print(f"Mehuvarasto: {mehua}")
 
-    print(f"Olutvarasto: {olutta}")
-    print("olutta.ota_varastosta(1000.0)")
-    saatiin = olutta.ota_varastosta(1000.0)
-    print(f"saatiin {saatiin}")
-    print(f"Olutvarasto: {olutta}")
-
-    print(f"Mehuvarasto: {mehua}")
-    print("mehua.otaVarastosta(-32.9)")
-    saatiin = mehua.ota_varastosta(-32.9)
-    print(f"saatiin {saatiin}")
-    print(f"Mehuvarasto: {mehua}")
+    print(dedent(
+        f"""\
+        Mehuvarasto: {mehua}
+        Olutvarasto: {olutta}
+        olutta.ota_varastosta(1000.0)
+        saatiin {olutta.ota_varastosta(1000.0)}
+        Olutvarasto: {olutta}
+        Mehuvarasto: {mehua}
+        mehua.otaVarastosta(-32.9)
+        saatiin {mehua.ota_varastosta(-32.9)}
+        Mehuvarasto: {mehua}"""
+    ))
 
 
 if __name__ == "__main__":
